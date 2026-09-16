f6e55b19a0d42f16ee1fcca311b477817476c2f1
KingSid29 <siddhantmittal103@gmail.com>
2026-09-16 16:08:26 +0100

    homework

--- lesson_5_homework.py
+++ lesson_5_homework.py
@@ -3,4 +3,4 @@ temperature = 24
 if temperature >= 19:
     print("It is warm enough to wear light clothes!")
 else:
-    print("It is too cold. Wear a jacket or pullover.")
+    print("It is too cold. Wear a jacket .")
