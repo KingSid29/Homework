5b3e8678f49f0f2c3b9eff8116114045121f1a38
KingSid29 <siddhantmittal103@gmail.com>
2026-09-22 17:38:09 +0100

    homework

---
+++ lesson_7_homework.py
@@ -0,0 +1,8 @@
+age = int(input("What is your age"))
+
+if age > 20:
+    print("Your too old to take the class.")
+elif age < 10:
+    print("Your too young to take class.")
+else:
+    print("You can take class")
